a31114f4ec966834eb729f86aab0faefeb59393d
sankarsharn <sankarsharnrastogi@gmail.com>
2025-02-08 09:52:47 +0530

    fixing

--- nextJS.project/next_blog/src/middleware.js
+++ nextJS.project/next_blog/src/middleware.js
@@ -1,9 +1,10 @@
-import { authMiddleware } from '@clerk/nextjs'
+import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-    publicRoutes: ["/" , "/api/webhooks"]
+    publicRoutes: ["/", "/api/webhooks"],
+    ignoredRoutes: ["/", "/api/webhooks"],
 });
 
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc).(*)"],
+    matcher: ["/((?!api/webhooks|$).*)"],
 };
